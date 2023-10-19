9279c89a33ab66718b7bd1996da990102df73880
James Morrissey <jpmorrissey11@gmail.com>
2023-10-19 07:44:24 -0400

    reorganize data generation

--- examples/it/index.py
+++ examples/it/index.py
@@ -1,6 +1,8 @@
 import itertools
 from collections import Counter
+from typing import List, Tuple
 
+import pandas as pd
 import pinecone
 import tensorflow.keras.backend as K
 from tqdm import tqdm
@@ -15,9 +17,19 @@ def chunks(iterable, batch_size=100):
         chunk = tuple(itertools.islice(it, batch_size))
 
 
+def generate_upload_items(embeddings, df: pd.DataFrame) -> List[Tuple]:
+    print("Generating items to upload...")
+    items_to_upload = []
+    for i, embedding in tqdm(zip(df.iterrows(), embeddings), total=len(embeddings)):
+        benign_or_attack = i[1]["Label"][:3]
+        items_to_upload.append((benign_or_attack + "_" + str(i[0]), embedding.tolist()))
+    return items_to_upload
+
+
 def upsert_batches(items_to_upload, index: pinecone.Index):
+    print("Upserting batches...")
     NUMBER_OF_ITEMS = len(items_to_upload)
-    for batch in chunks(items_to_upload[:NUMBER_OF_ITEMS], 50):
+    for batch in tqdm(chunks(items_to_upload[:NUMBER_OF_ITEMS], 50)):
         index.upsert(vectors=batch)
 
 
@@ -27,40 +39,3 @@ def batch_query_results(test_vector, index: pinecone.Index):
         query_res = index.query(xq, top_k=50)
         query_results.append(query_res)
     return query_results
-
-
-def get_predictions(data_sample, embedding_model, index, batch_size=100):
-    y_true = []
-    y_pred = []
-
-    for i in tqdm(range(0, len(data_sample), batch_size)):
-        test_data = data_sample.iloc[i : i + batch_size, :]
-        # Create vector embedding using the model
-        test_vector = embedding_model.predict(K.constant(test_data.iloc[:, :-1]))
-
-        # Query using the vector embedding
-        query_results = []
-        for xq in test_vector.tolist():
-            query_res = index.query(xq, top_k=50)
-            query_results.append(query_res)
-
-        for label, res in zip(test_data.Label.values, query_results):
-            y_true.append(add_to_true_list(label))
-            counter = Counter(match.id.split("_")[0] for match in res.matches)
-            y_pred.append(add_to_pred_list(counter))
-
-    return y_true, y_pred
-
-
-def add_to_true_list(label: str):
-    if label == "Benign":
-        return 0
-    else:
-        return 1
-
-
-def add_to_pred_list(counter: Counter):
-    if counter["Bru"] or counter["SQL"]:
-        return 1
-    else:
-        return 0
--- examples/it/main.py
+++ examples/it/main.py
@@ -5,6 +5,7 @@ from collections import Counter
 from typing import List, Tuple
 
 import pandas as pd
+import pinecone
 import tensorflow.keras.backend as K
 from tensorflow import keras
 from tensorflow.keras.models import Model
@@ -12,18 +13,10 @@ from tqdm import tqdm
 
 sys.path.append("/Users/jamesmorrissey/Github/plutonium/src")
 
-from index import get_predictions, upsert_batches
+from index import generate_upload_items, upsert_batches
 from metrics import per_class_accuracy, plot_confusion_matrix, print_metrics
 
 from core.cloud.pc import get_pinecone_index, init_pinecone_connection
-
-
-def generate_items(embeddings, df: pd.DataFrame) -> List[Tuple]:
-    items_to_upload = []
-    for i, embedding in tqdm(zip(df.iterrows(), embeddings), total=len(embeddings)):
-        benign_or_attack = i[1]["Label"][:3]
-        items_to_upload.append((benign_or_attack + "_" + str(i[0]), embedding.tolist()))
-    return items_to_upload
 
 
 def build_embedding_model(args):
@@ -37,14 +30,44 @@ def build_embedding_model(args):
     return intermediate_layer_model
 
 
+def get_predictions(data_sample, embedding_model, index, batch_size=100):
+    y_true = []
+    y_pred = []
+    for i in tqdm(range(0, len(data_sample), batch_size)):
+        test_data = data_sample.iloc[i : i + batch_size, :]
+        test_vector = embedding_model.predict(K.constant(test_data.iloc[:, :-1]))
+        query_results = []
+        for xq in test_vector.tolist():
+            query_res = index.query(xq, top_k=50)
+            query_results.append(query_res)
+        for label, res in zip(test_data.Label.values, query_results):
+            y_true.append(add_to_true_list(label))
+            counter = Counter(match.id.split("_")[0] for match in res.matches)
+            y_pred.append(add_to_pred_list(counter))
+
+    return y_true, y_pred
+
+
+def add_to_true_list(label: str):
+    if label == "Benign":
+        return 0
+    else:
+        return 1
+
+
+def add_to_pred_list(counter: Counter):
+    if counter["Bru"] or counter["SQL"]:
+        return 1
+    else:
+        return 0
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", type=str, required=True)
     parser.add_argument(
         "--model_dir", type=str, default="/Users/jamesmorrissey/Github/plutonium/models"
     )
-    parser.add_argument("--index_name", type=str, default="it-threats")
-    parser.add_argument("--dimension", type=int, default=128)
     parser.add_argument("--metric", type=str, default="euclidean")
     parser.add_argument("--create_index", action="store_true")
     parser.add_argument("--upsert_data", action="store_true")
@@ -52,15 +75,15 @@ def parse_args():
     return args
 
 
+CREATE_INDEX = False
 if __name__ == "__main__":
     args = parse_args()
     init_pinecone_connection()
-    index = get_pinecone_index(
-        index_name=args.index_name,
-        dimension=args.dimension,
-        metric=args.metric,
-        create_index=args.create_index,
-    )
+    if CREATE_INDEX:
+        pinecone.create_index(name="it-threats", dimension=128, metric="euclidean")
+        index = pinecone.Index("it-threats")
+    else:
+        index = pinecone.Index("it-threats")
 
     data_23_cleaned = pd.read_csv(os.path.join(args.data_dir, "result23.csv"))
     data_22_cleaned = pd.read_csv(os.path.join(args.data_dir, "result22022018.csv"))
@@ -68,16 +91,14 @@ if __name__ == "__main__":
 
     embedding_model = build_embedding_model(args)
     embeddings = embedding_model.predict(K.constant(data_23_cleaned.iloc[:, :-1]))
-    items_to_upload = generate_items(embeddings, df=data_23_cleaned)
+    items_to_upload = generate_upload_items(embeddings, df=data_23_cleaned)
 
     if args.upsert_data:
         upsert_batches(items_to_upload, index)
         items_to_upload.clear()
 
-    print(index.describe_index_stats())
-
     y_true, y_pred = get_predictions(
-        data_sample=data_sample, embedding_model=embedding_model
+        data_sample=data_sample, embedding_model=embedding_model, index=index
     )
     plot_confusion_matrix(y_true=y_true, y_pred=y_pred)
     print_metrics(y_true=y_true, y_pred=y_pred)
